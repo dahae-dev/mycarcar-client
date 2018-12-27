88224da11a2af5d611c17acca8419d46ab95e9ff
dahaekim88 <dhkim1014@gmail.com>
2018-12-27 19:57:59 +0900

    mapping each components

---
+++ src/Components/Content/index.tsx
@@ -0,0 +1,5 @@
+import Home from "./Home/Home";
+import RegisterForm from "./User/Register/RegisterForm";
+import LoginForm from "./User//Login/LoginForm";
+
+export { Home, RegisterForm, LoginForm };
