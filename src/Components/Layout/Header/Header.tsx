f0451e6559100691e5d6f6851580bfce6c6f0537
dahaekim88 <dhkim1014@gmail.com>
2018-12-28 15:24:29 +0900

    render index view when logo image clicked

--- src/Components/Layout/Header/Header.tsx
+++ src/Components/Layout/Header/Header.tsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import "./Header.css";
 import logo from "../../../assets/img/logo.png";
 
-class Header extends React.Component {
+interface IHeaderProps {
+  handleClick: (comp: string) => void;
+}
+
+class Header extends React.Component<IHeaderProps, {}> {
+  constructor(props: IHeaderProps) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="my-header">
-        <img className="logo-image" src={logo} alt="mycarcar logo" />
+        <a onClick={() => this.props.handleClick("Home")}>
+          <img className="logo-image" src={logo} alt="mycarcar logo" />
+        </a>
         {/* menu toggle button 삽입 */}
       </div>
     );
