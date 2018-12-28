fb5a9ccfa4b61f9a03083621a99a376ee9ed1120
dahaekim88 <dhkim1014@gmail.com>
2018-12-28 15:37:36 +0900

    handle logout & authentication conditional rendering

--- src/Components/Layout/SideBar/Status/Status.tsx
+++ src/Components/Layout/SideBar/Status/Status.tsx
@@ -1,16 +1,39 @@
 import * as React from "react";
 import "./Status.css";
+import axios from "axios";
 
 interface IStatusProps {
   isSignedIn: boolean;
-  handleClickLogin: () => void;
-  handleClickLogout: () => void;
-  handleClickRegister: () => void;
+  handleClick: (comp: string) => void;
+  handleLogin: (result: boolean) => void;
+  handleLogout: () => void;
 }
+
+const config: object = {
+  headers: { "x-access-token": localStorage.getItem("x-access-token") }
+};
 
 class Status extends React.Component<IStatusProps, {}> {
   constructor(props: IStatusProps) {
     super(props);
+
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    axios
+      .get(`http://localhost:5000/api/logout`, config)
+      .then(res => {
+        const result = res.data.isSignedOut;
+        if (result) {
+          localStorage.removeItem("x-access-token");
+          this.props.handleLogout();
+          setTimeout(() => {
+            this.props.handleClick("AfterAuth");
+          }, 500);
+        }
+      })
+      .catch((err: Error) => console.log(err));
   }
 
   render() {
@@ -18,25 +41,34 @@ class Status extends React.Component<IStatusProps, {}> {
       <div className="status">
         {!this.props.isSignedIn ? (
           <div className="btn-wrapper">
-            <button className="btn-user" type="button" onClick={() => this.props.handleClickLogin()}>
+            <button className="btn-user" type="button" onClick={() => this.props.handleClick("Login")}>
               <i className="btn-icon fa fa-sign-in" />
               <span>로그인</span>
             </button>
           </div>
         ) : (
           <div className="btn-wrapper">
-            <button className="btn-user" type="button" onClick={() => this.props.handleClickLogout()}>
+            <button className="btn-user" type="button" onClick={() => this.handleLogout()}>
               <i className="btn-icon fa fa-sign-in" />
               <span>로그아웃</span>
             </button>
           </div>
         )}
-        <div className="btn-wrapper">
-          <button className="btn-user" type="button" onClick={() => this.props.handleClickRegister()}>
-            <i className="btn-icon fa fa-user" />
-            <span>회원가입</span>
-          </button>
-        </div>
+        {!this.props.isSignedIn ? (
+          <div className="btn-wrapper">
+            <button className="btn-user" type="button" onClick={() => this.props.handleClick("Register")}>
+              <i className="btn-icon fa fa-user" />
+              <span>회원가입</span>
+            </button>
+          </div>
+        ) : (
+          <div className="btn-wrapper">
+            <button className="btn-user" type="button" onClick={() => this.props.handleClick("Edit")}>
+              <i className="btn-icon fa fa-user" />
+              <span>정보수정</span>
+            </button>
+          </div>
+        )}
       </div>
     );
   }
