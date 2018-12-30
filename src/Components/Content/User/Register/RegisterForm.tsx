f3f511c93bed1519b2552f6ffa9a434cea43cb49
dahaekim88 <dhkim1014@gmail.com>
2018-12-30 17:31:38 +0900

    add pre-loader

--- src/Components/Content/User/Register/RegisterForm.tsx
+++ src/Components/Content/User/Register/RegisterForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import axios from "axios";
 import logo from "../../../../assets/img/logo_basic.png";
+import loader from "../../../../assets/preloader/Spinner.gif";
 import "./RegisterForm.css";
 
 interface IRegisterProps {
@@ -72,91 +73,95 @@ class RegisterForm extends React.Component<IRegisterProps, IRegisterState> {
         }, 1000);
       })
       .catch((err: Error) => {
-        console.log(err.message);
         this.setState({ loading: false, error: "이미 가입된 회원입니다." });
       });
   }
 
   render() {
     const { id, pw, pwdcheck, name, email, phone, loading, error } = this.state;
+
     return (
       <div className="register-form-container">
-        <div>
-          <div className="register-logo">
-            <img src={logo} />
-          </div>
-          <div className="register-form-box">
-            <div className="register-title">
-              <i className="fa fa-user" />
-              회원가입
+        {loading ? (
+          <img className="pre-loader" src={loader} />
+        ) : (
+          <div>
+            <div className="register-logo">
+              <img src={logo} />
             </div>
-            <hr />
-            <form className="register-form-input" method="post" onSubmit={this.handleSubmit}>
-              <label htmlFor="id">아이디</label>
-              <input
-                type="text"
-                name="u_id"
-                id="id"
-                placeholder="아이디"
-                required
-                value={id}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="pw">비밀번호</label>
-              <input
-                type="password"
-                name="u_pw"
-                id="pw"
-                placeholder="비밀번호"
-                required
-                value={pw}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="pwdcheck">비밀번호 확인</label>
-              <input
-                type="password"
-                id="pwdcheck"
-                placeholder="비밀번호 확인"
-                required
-                value={pwdcheck}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="name">이름</label>
-              <input
-                type="text"
-                name="u_name"
-                id="name"
-                placeholder="이름"
-                required
-                value={name}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="email">이메일</label>
-              <input
-                type="email"
-                name="u_email"
-                id="email"
-                placeholder="email@email.com"
-                required
-                value={email}
-                onChange={this.handleChange}
-              />
-              <label id="phone">휴대폰번호</label>
-              <input
-                type="tel"
-                pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
-                name="u_phone"
-                id="phone"
-                placeholder="000-0000-0000"
-                required
-                value={phone}
-                onChange={this.handleChange}
-              />
-              <div className="register-error-msg">{error}</div>
-              <input type="submit" id="btn-register" value="SIGN UP" disabled={loading} />
-            </form>
+            <div className="register-form-box">
+              <div className="register-title">
+                <i className="fa fa-user" />
+                회원가입
+              </div>
+              <hr />
+              <form className="register-form-input" method="post" onSubmit={this.handleSubmit}>
+                <label htmlFor="id">아이디</label>
+                <input
+                  type="text"
+                  name="u_id"
+                  id="id"
+                  placeholder="아이디"
+                  required
+                  value={id}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="pw">비밀번호</label>
+                <input
+                  type="password"
+                  name="u_pw"
+                  id="pw"
+                  placeholder="비밀번호"
+                  required
+                  value={pw}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="pwdcheck">비밀번호 확인</label>
+                <input
+                  type="password"
+                  id="pwdcheck"
+                  placeholder="비밀번호 확인"
+                  required
+                  value={pwdcheck}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="name">이름</label>
+                <input
+                  type="text"
+                  name="u_name"
+                  id="name"
+                  placeholder="이름"
+                  required
+                  value={name}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="email">이메일</label>
+                <input
+                  type="email"
+                  name="u_email"
+                  id="email"
+                  placeholder="email@email.com"
+                  required
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <label id="phone">휴대폰번호</label>
+                <input
+                  type="tel"
+                  pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+                  name="u_phone"
+                  id="phone"
+                  placeholder="000-0000-0000"
+                  required
+                  value={phone}
+                  onChange={this.handleChange}
+                />
+                <div className="register-error-msg">{error}</div>
+                <input type="submit" id="btn-register" value="SIGN UP" disabled={loading} />
+              </form>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
