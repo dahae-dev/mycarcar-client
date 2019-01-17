9ccdc0d55ae5f00082fea5bb4a9d853919b176e2
dahaekim88 <dhkim1014@gmail.com>
2019-01-17 10:30:16 +0900

    Fix import path

--- src/Components/App/Layout/Main/Main.tsx
+++ src/Components/App/Layout/Main/Main.tsx
@@ -3,15 +3,16 @@ import "./Main.css";
 import React from "react";
 
 import Home from "./Home/Home";
-import LoginForm from "./Login/LoginForm";
-import EditForm from "./EditAccount/EditForm";
-import RegisterForm from "./RegisterForm/RegisterForm";
-import RegisterTerms from "./RegisterTerms/RegisterTerms";
+
+import LoginForm from "./UserAccount/Login/LoginForm";
+import RegisterTerms from "./UserAccount/RegisterTerms/RegisterTerms";
+import RegisterForm from "./UserAccount/RegisterForm/RegisterForm";
+import EditForm from "./UserAccount/EditAccount/EditForm";
 import Rental from "./Rental/Rental";
 import SuperAdmin from "./SuperAdmin/SuperAdmin";
+import { EstimateList } from "./Estimate/EstimateList/EstimateList";
+import { EstimateForm } from "./Estimate/EstimateForm/EstimateForm";
 import { NotFound } from "./NotFound/NotFound";
-import { EstimateForm } from "./EstimateForm/EstimateForm";
-import { EstimateList } from "./EstimateList/EstimateList";
 import { IHandlePage } from "../../App";
 
 interface IMainProps {
